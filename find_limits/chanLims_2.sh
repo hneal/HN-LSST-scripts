a68273eea21e882303e9b6e6677b460abde4cec6
Homer A. Neal Jr <homer@lsst-it01.slac.stanford.edu>
2023-08-02 13:07:06 -0700

    generalized channel list generation and adjusted sampling

--- find_limits/chanLims_2.sh
+++ find_limits/chanLims_2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 chan=$1
 #echo $chan
-~/yupy ~/stutils/trender.py --text --start "Sun Jun 18 00:00:00 PDT 2023" --duration 200000 -- $chan | awk 'BEGIN{mn=1.0e+36;mx=-1.0e+36; nvals=0; sum=0.0; sumsq=0.0} /\//{mx=($2>mx?$2:mx); mn=($2<mn?$2:mn); sum=sum+$2; sumsq=sumsq+$2*$2; nvals=nvals+1} END{print $(NF-1),mn,mx,sum,sumsq,nvals,sqrt( (sumsq-(sum*sum)/nvals)/(nvals-1) )}'
+#200000
+~/yupy ~/stutils/trender.py --text --start "Jun 23 00:00:00 PDT 2023" --duration 200000 --timebins 25 -- $chan | awk 'BEGIN{mn=1.0e+36;mx=-1.0e+36; nvals=0; sum=0.0; sumsq=0.0} /\//{mx=($2>mx?$2:mx); mn=($2<mn?$2:mn); sum=sum+$2; sumsq=sumsq+$2*$2; nvals=nvals+1} END{sqarg=(sumsq-(sum*sum)/nvals)/(nvals-1);print $(NF-1),mn,mx,sum,sumsq,nvals,sqrt((sqarg>0)*sqarg)}'
--- find_limits/genLimsForSubSys.sh
+++ find_limits/genLimsForSubSys.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 subsys=$1
-cfs cat config/$subsys/Limits/defaultInitial.properties | awk -F "/" '{print $1"/"$2}' | uniq | xargs -n 1 ./chanLims_2.sh | tee $subsys.dat
+props=$2
+#cfs cat config/$subsys/Limits/$props.properties | awk -F "/" '{print $1"/"$2}' | uniq | xargs -n 1 ./chanLims_2.sh | tee $subsys.dat
+cfs cat config/RebPowerSupply/Limits/rebpower.properties | grep -v "^#" | awk '{print $1}' | sed 's/\/limit..//' | sed 's/\/warn..//' | uniq | xargs -n 1 ./chanLims_2.sh | tee $subsys.dat
