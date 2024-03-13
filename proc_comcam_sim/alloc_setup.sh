993dc46ac20a3abe157bdab99a2f3005ddcf4832
Homer Neal <homer@slac.stanford.edu>
2024-03-13 10:24:45 -0700

    small tweaks including removing the memory multiplier

--- proc_comcam_sim/alloc_setup.sh
+++ proc_comcam_sim/alloc_setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-export LSST_TAG=w_2024_08
-lsstsw_root=/sdf/group/rubin/sw
-source ${lsstsw_root}/loadLSST.bash
-setup -v lsst_distrib -t ${LSST_TAG}
+#export LSST_TAG=w_2024_10
+#lsstsw_root=/sdf/group/rubin/sw
+#source ${lsstsw_root}/loadLSST.bash
+#setup -v lsst_distrib -t ${LSST_TAG}
 
 # Loop for a long time, executing "allocateNodes auto" every 10 minutes.
 for i in {1..500}
 do
-    allocateNodes.py --auto --account rubin:developers -n 200 -m 4-00:00:00 -q milano -g 240 s3df
+    allocateNodes.py --auto --account rubin:production -n 400 -m 4-00:00:00 -q milano -g 240 s3df
+#    allocateNodes.py --auto --account rubin:developers -n 200 -m 4-00:00:00 -q milano -g 240 s3df
 #    allocateNodes.py --auto --account rubin:developers -n 200 -m 4-00:00:00 -q milano,roma -g 240 s3df
     sleep 60
 done
