e68ef657857a6d27610273f53e673cbfbd06ec64
Homer Neal <homer@slac.stanford.edu>
2025-02-07 10:50:22 -0800

    updated input collection specification and added node allocation provisioning specification

--- LSSTCam-Nightly-Validation/alloc_setup.sh
+++ LSSTCam-Nightly-Validation/alloc_setup.sh
@@ -7,8 +7,10 @@
 # Loop for a long time, executing "allocateNodes auto" every 10 minutes.
 for i in {1..10000}
 do
-    allocateNodes.py --auto --account rubin:production -n 15 -c 16 -m 4-00:00:00 -q milano -g 60 s3df
-# -maxjobs 1500
+#    allocateNodes.py --auto --account rubin:production -n 15 -c 16 -m 4-00:00:00 -q milano -g 60 s3df
+#    allocateNodes.py --auto --account rubin:developers -n 15 -c 16 -m 4-00:00:00 -q milano -g 60 s3df
+    allocateNodes.py --auto --account rubin:developers -n 15 -m 1-00:00:00 -q milano -g 240 s3df
+    # -maxjobs 1500
 #    allocateNodes.py --auto --account rubin:developers -n 200 -m 4-00:00:00 -q milano -g 240 s3df
 #    allocateNodes.py --auto --account rubin:developers -n 200 -m 4-00:00:00 -q milano,roma -g 240 s3df
     sleep 120
